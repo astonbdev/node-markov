53e8bba22e24411f7209e023ef01c56320f41307
Sam Courtenay <sam.m.courtenay@gmail.com>
2022-02-15 17:29:42 -0800

    added test for branches of our chain

--- markov.test.js
+++ markov.test.js
@@ -20,3 +20,36 @@ describe("testing getChains()", function (){
 
     //separate test for bad data
 });
+
+describe("testing getText()", function (){
+
+    // test("test getText() no branches", function () {
+        // let markov = new MarkovMachine("The cat in the hat.");
+        // chains = {
+        //     "The": ["cat"],
+        //     "cat": ["in"],
+        //     "in": ["the"],
+        //     "the": ["hat."],
+        //     "hat.": [null]
+        // }
+
+        // expect(markov.chains).toEqual(chains);
+    // });
+
+    test("test getText() with branches", function () {
+        let markov = new MarkovMachine("The cat is in the hat. The cat is the cat. The hat is a cat.");
+
+        let markovSentence = markov.getText();
+        let markovSentenceArr = markovSentence.split(" ");
+
+        for (let i = 0; i< markovSentenceArr.length-2; i++){
+
+            let currWord = markovSentenceArr[i];
+            let nextWord = markovSentenceArr[i+1];
+
+            expect(markov.chains[currWord].includes(nextWord)).toEqual(true);
+        }
+    });
+
+    //separate test for bad data
+});
